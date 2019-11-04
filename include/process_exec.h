44120a515dbebc867e02921aee043ff774821029
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2019-11-04 19:13:39 -0300

    Finished exec implementation and refactored code

--- include/process_exec.h
+++ include/process_exec.h
@@ -1,9 +1,9 @@
 #ifndef PROCESS_EXEC_H
 #define PROCESS_EXEC_H
 
-int fork_process(char **, int);
+int fork_process(char **);
 int execute(char **);
-int execv_mod(char **);
-int check_background_execution(char **);
+int execv_mod(char*, char **);
+int is_background_exec(char **);
 
 #endif
--- src/process_exec.c
+++ src/process_exec.c
@@ -16,126 +16,186 @@
 #include <dirent.h>
 #include <errno.h>
 
-#define TRUE 1
-#define FALSE 0
-
-#define MAX_SIZE 1024
-
-int execv_mod(char *argv[])
+int is_background_exec(char **args)
 {
+    int token_pos = 0;
+
+    while (args[token_pos] != NULL)
+    {
+        if (strcmp(args[token_pos], "&") == 0 && args[token_pos + 1] == NULL) //si & es el ultimo token
+        {
+            /* Remove & and set pointer to NULL */
+            char *temp = args[token_pos];
+            args[token_pos] = NULL;
+            free(temp);
+
+            return 1; //TRUE
+        }
+        token_pos++;
+    }
+    return 0; //FALSE
+}
+
+int execv_mod(char *file, char *argv[])
+{
+
     /* 
         Trato de ubicar al ejecutable en la siguiente orden:
-        1- si contiene / al inicio, es decir, es un camino a un archivo
+        1- si contiene / al, es decir, es un camino a un archivo
+        caso contrario es el nombre de un ejecutable que deberia estar ubucado en:
         2- me fijo en el directorio actual
         3- trato de ubicar el archivo los los directorios de PATH
      */
 
-    //1
-    if (strchr(argv[0], '/') != NULL)
+    char buffer[1024];
+
+    if (strchr(file, '/') != NULL)
     {
-        execv(argv[0], argv); //if execv returns then an error occurred
-        return -1;
+        execv(file, argv); //if execv returns then an error occurred
+    }
+    else
+    {
+
+        char *path = getenv("PATH");
+        if (path == NULL)
+        {
+            fprintf(stderr, "PATH variable does not exist");
+            return 1;
+        }
+
+        size_t len = strlen(file) + 1;
+        size_t pathlen = strlen(path);
+
+        char *name = memcpy(buffer + pathlen + 1, file, len); //copy file name to top of buffer
+        *--name = '/';                                        //add preceeding slash to name
+
+        /* Try for current directory */
+        char *cur_dir = name - 1;
+        *cur_dir = '.';
+
+        execv(cur_dir, argv);
+
+        /* Try for directories in PATH */
+        char *p = path;
+        int got_eacces = 0;
+
+        do
+        {
+            char *startp;
+            path = p;
+            p = strchr(path, ':');
+            if (!p)
+            {
+                p = strchr(path, '\0');
+            }
+            if (p == path)
+            {
+                /* Two adjacent colons, or a colon at the beginning or the end
+				   of `PATH' means to search the current directory.  */
+                startp = name + 1;
+            }
+            else
+            {
+                startp = memcpy(name - (p - path), path, p - path);
+            }
+
+            execv(startp, argv); //returns only on error.
+
+            switch (errno)
+            {
+            case EACCES: //permission denied, diagnose we found ti but couldnt execute it
+                got_eacces = 1;
+            case ENOENT:
+            case ESTALE:
+            case ENOTDIR:
+            case ENODEV:
+            case ETIMEDOUT:
+            case ENOEXEC: //these errors indicate file missing or not executable by us. so we want to try the next path
+                break;
+            default: // other errors mean we found but something went wrong executing it, return error to
+                perror("execv");
+                return errno;
+            }
+
+        } while (*p++ != '\0');
+
+        if (got_eacces)
+        {
+            return EACCES;
+        }
     }
 
-    //2
-    char buffer[MAX_SIZE];
-    char *test = getcwd(buffer, MAX_SIZE);
-    if (test == NULL)
-    {
-        printf("ERR\n");
-    }
-
-    strcat(buffer, "/");
-    strcat(buffer, argv[0]);
-    execv(buffer, argv);
-
-    //3
-    char *path = getenv("PATH");
-    char *subpath = NULL;
-    char *delim = ":";
-
-    subpath = strtok(path, delim);
-
-    memset(buffer, 0, sizeof buffer);
-
-    while (subpath != NULL)
-    {
-        strcat(buffer, subpath);
-        strcat(buffer, "/");
-        strcat(buffer, argv[0]);
-
-        execv(buffer, argv);
-
-        memset(buffer, 0, sizeof buffer);
-        subpath = strtok(NULL, delim);
-    }
-
-    return -1;
+    fprintf(stderr, "%s: command not found\n", file);
+    return errno;
 }
 
-int fork_process(char **args, int is_background_proc)
+int fork_process(char **args)
 {
+
+    int background = is_background_exec(args);
 
     pid_t child_pid;
     int child_status;
     child_pid = fork();
-    int fd;
 
     if (child_pid == (pid_t)0) //child
     {
-        int inputRedir = FALSE;
-        int outputRedir = FALSE;
-
-        char input[MAX_SIZE];
-        char output[MAX_SIZE];
-
-        for (int i = 0; args[i] != NULL; i++)
+        /* I/O redirection */
+        int fd;
+        int i = 0;
+        while (args[i])
         {
-            if (strcmp(args[i], "<") == 0)
+            if (*args[i] == '<' && args[i + 1])
             {
-                args[i] = NULL;
-                strcpy(input, args[i + 1]);
-                inputRedir = TRUE;
+                if ((fd = open(args[i + 1], O_RDONLY)) == -1)
+                {
+                    perror(args[i + 1]);
+                    exit(EXIT_FAILURE);
+                }
+                dup2(fd, STDIN_FILENO);
+                close(fd);
+                while (args[i])
+                {
+                    args[i] = args[i + 2];
+                    i++;
+                }
+                break;
             }
-
-            if (strcmp(args[i], ">") == 0)
+            else if (*args[i] == '>' && args[i + 1])
             {
-                args[i] = NULL;
-                strcpy(output, args[i + 1]);
-                outputRedir = TRUE;
+                //int = fd = open(args[i + 1],
+                //    O_WRONLY | O_CREAT, + TRUNCATE TO 0 EQUIV
+                //    S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+                if ((fd = creat(args[i + 1], S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1)
+                {
+                    perror(args[i + 1]);
+                    exit(EXIT_FAILURE);
+                }
+                dup2(fd, STDOUT_FILENO);
+                // dup2(fd, 2); ADD STDOUT_ERROR?
+                close(fd);
+                while (args[i])
+                {
+                    args[i] = args[i + 2];
+                    i++;
+                }
+                break;
             }
+            i++;
         }
 
-        if (inputRedir)
-        {
-            int fd0 = open(input, O_RDONLY);
-            dup2(fd0, STDIN_FILENO);
-            close(fd0);
-        }
-
-        if (outputRedir)
-        {
-            int fd1 = creat(output, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-            dup2(fd1, STDOUT_FILENO);
-            close(fd1);
-        }
-
-        if (is_background_proc && !outputRedir)
-        {
-            fd = open("/dev/null", O_WRONLY | O_CREAT);
-            dup2(fd, 1);
-        }
-
+        /* After redirecting, call the echo function */
         if (echo_func(args) == 0)
         {
-            exit(EXIT_SUCCESS);
+            _exit(EXIT_SUCCESS); //prevents stdio buffers to be flushed twice and temporary files might be removed
         }
 
-        execv_mod(args);
+        execv_mod(args[0], args);
         abort();
     }
 
-    else if (!is_background_proc)
+    else if (!background)
     {
         while (wait(&child_status) != child_pid)
         {
@@ -165,21 +225,5 @@ int execute(char **args)
         return 0;
     }
 
-    int token_pos = 0;
-
-    while (args[token_pos] != NULL)
-    {
-        if (strcmp(args[token_pos], "&") == 0 && args[token_pos + 1] == NULL) //si & es el ultimo token
-        {
-            /* Remove & and set pointer to NULL */
-            char *temp = args[token_pos];
-            args[token_pos] = NULL;
-            free(temp);
-
-            return fork_process(args, TRUE);
-        }
-        token_pos++;
-    }
-
-    return fork_process(args, FALSE);
+    return fork_process(args);
 }
