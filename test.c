43ceff81782d941e6f1a8b56897ba00c1f37e6ef
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2019-11-05 21:11:02 -0300

    updated test files

--- test.c
+++ test.c
@@ -1,12 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int main(){
+int main()
+{
 
-    for (size_t i = 0; i < 100; i++)
+    for (size_t i = 0; i < 5; i++)
     {
         printf("%ld\n", i);
     }
-    
+
+    sleep(10);
+
+    for (size_t i = 5; i < 10; i++)
+    {
+        printf("%ld\n", i);
+    }
     return 0;
 }
